d0d67bcdd2e4272d59c46f5396f3d7d7d3b7a993
Ivo-DiDutch <52910305+Ivo-DiDutch@users.noreply.github.com>
2019-10-23 21:55:42 +0200

    Update script license

--- Tween.cs
+++ Tween.cs
@@ -1,8 +1,8 @@
 /*
 The MIT License (MIT)
-Copyright (c) 2016 Digital Ruby, LLC
-http://www.digitalruby.com
-Created by Jeff Johnson
+Copyright (c) 2019 WobbleSoft
+https://www.wobble-soft.com
+Created by Ivo Ketelaar
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
